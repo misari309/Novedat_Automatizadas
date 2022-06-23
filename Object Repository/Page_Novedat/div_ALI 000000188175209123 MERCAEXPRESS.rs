47375ed64a367a03a7dab8c7ea1df5b792cce6b7
misari309 <90524159+misari309@users.noreply.github.com>
2022-06-23 16:24:32 -0500

    R_M_A_Bloquear

---
+++ Object Repository/Page_Novedat/div_ALI 000000188175209123 MERCAEXPRESS.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_ALI 000000188175209123 MERCAEXPRESS</name>
+   <tag></tag>
+   <elementGuidId>20763d4d-026a-4434-b5be-ce9108d8cf09</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='bodyHtml']/div/section/div/div[2]/div/app-obligations-holder-list/div/a/div/div[2]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.cursorPointerLink.ng-star-inserted > div.row.main-box > div.col-10.box2</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>70e239dc-d801-4eb5-aa59-0710ed5b3798</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col-10 box2</value>
+      <webElementGuid>05b920ab-cc43-4d1e-82cd-229533819fb3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>ALI 000000188175209123 MERCAEXPRESS    </value>
+      <webElementGuid>e1685836-9350-4e3d-a8ee-0e5f7ad44da0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;bodyHtml&quot;)/div[@class=&quot;ng-star-inserted&quot;]/section[@class=&quot;obligacion-status typeLetterBody&quot;]/div[@class=&quot;page-card&quot;]/div[@class=&quot;obligacion-result ng-star-inserted&quot;]/div[@class=&quot;row&quot;]/app-obligations-holder-list[@class=&quot;appObligList ng-star-inserted&quot;]/div[@class=&quot;col-sm-12 col-12 typeLetterBody ng-star-inserted&quot;]/a[@class=&quot;cursorPointerLink ng-star-inserted&quot;]/div[@class=&quot;row main-box&quot;]/div[@class=&quot;col-10 box2&quot;]</value>
+      <webElementGuid>9fd75052-1e80-45a3-a382-19a0043c0ba1</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='bodyHtml']/div/section/div/div[2]/div/app-obligations-holder-list/div/a/div/div[2]</value>
+      <webElementGuid>70b7c2ef-4e80-445d-b646-27173315f584</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='F.PERMANENCIA'])[2]/following::div[6]</value>
+      <webElementGuid>5dd77110-901b-4799-8c34-ed3ea9870e76</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='TIPO | NÚMERO DE OBLIGACIÓN | ENTIDAD'])[2]/following::div[7]</value>
+      <webElementGuid>9b0e774e-137c-4e3d-81a5-61d83277ffb7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='×'])[1]/preceding::div[7]</value>
+      <webElementGuid>1478276e-2f6c-4fd1-bacb-f8e73c55b5db</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a/div/div[2]</value>
+      <webElementGuid>c6dfb1fc-a210-4bea-9647-690be00a8d57</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'ALI 000000188175209123 MERCAEXPRESS    ' or . = 'ALI 000000188175209123 MERCAEXPRESS    ')]</value>
+      <webElementGuid>742268a7-0250-41b5-8c16-5e51e03acefd</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_Novedat/h3_ALI 000000188175209123 MERCAEXPRESS    B.rs
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_ALI 000000188175209123 MERCAEXPRESS    B</name>
+   <tag></tag>
+   <elementGuidId>976a014f-87ec-49d4-85d5-9c1e0206637f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='F.PERMANENCIA'])[2]/following::h3[1]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.cursorPointerLink > h3</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+      <webElementGuid>562db3c4-26c8-4c9c-9042-a9a92821ac0c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>ALI 000000188175209123 MERCAEXPRESS    B</value>
+      <webElementGuid>114a447e-2a4a-4f95-9da1-3dffce123ded</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/article[@class=&quot;main-content&quot;]/app-reactivation-account[@class=&quot;ng-star-inserted&quot;]/div[@class=&quot;ng-star-inserted&quot;]/section[@class=&quot;obligacion-status&quot;]/div[@class=&quot;page-card&quot;]/div[@class=&quot;obligacion-result ng-star-inserted&quot;]/div[@class=&quot;row&quot;]/app-obligations-holder-list2[@class=&quot;appObligList ng-star-inserted&quot;]/div[@class=&quot;col-sm-12 col-12 ng-star-inserted&quot;]/a[@class=&quot;cursorPointerLink&quot;]/h3[1]</value>
+      <webElementGuid>0956272a-80ad-478a-9e14-15cecd968ef2</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='F.PERMANENCIA'])[2]/following::h3[1]</value>
+      <webElementGuid>5d99c9b5-a27c-4e50-9ccc-67b13f75101f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='TIPO | NÚMERO DE OBLIGACIÓN | ENTIDAD'])[2]/following::h3[1]</value>
+      <webElementGuid>274f20c7-319e-44da-8620-e21985d2b4f3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a/h3</value>
+      <webElementGuid>af7ca6fb-1dfb-453a-a3d7-06316a34e561</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h3[(text() = 'ALI 000000188175209123 MERCAEXPRESS    B' or . = 'ALI 000000188175209123 MERCAEXPRESS    B')]</value>
+      <webElementGuid>237a4d6b-6549-4d5e-ad5e-8e3ab6aa4b7a</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
