206e2326e2063ab9ddc11913dc8db90e093a70a0
misari309 <90524159+misari309@users.noreply.github.com>
2022-07-06 17:46:31 -0500

    65. O_Masivas

---
+++ Object Repository/Page_Novedat/div_CVE 000000111699097121 VOTRE PASSION  S.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_CVE 000000111699097121 VOTRE PASSION  S</name>
+   <tag></tag>
+   <elementGuidId>6010ac46-f14f-4cbb-a533-9caff8fc8a09</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.col-10.box2 > div.row > div.col-10</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='F.PERMANENCIA'])[2]/following::div[8]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>370cb333-d622-40d7-b010-e63f2909735d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col-10</value>
+      <webElementGuid>2532989a-0b92-4157-8a27-94fbacf6ff12</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>CVE 000000111699097121 VOTRE PASSION  S</value>
+      <webElementGuid>c585886f-bb46-4b20-a46b-5c40e1cf6ca9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/article[@class=&quot;main-content&quot;]/app-dispute[@class=&quot;ng-star-inserted&quot;]/div[@class=&quot;ng-star-inserted&quot;]/section[@class=&quot;obligacion-status typeLetterBody&quot;]/div[@class=&quot;page-card&quot;]/div[@class=&quot;obligacion-result ng-star-inserted&quot;]/div[@class=&quot;row&quot;]/app-obligations-holder-list[@class=&quot;appObligList ng-star-inserted&quot;]/div[@class=&quot;col-sm-12 col-12 typeLetterBody ng-star-inserted&quot;]/a[@class=&quot;cursorPointerLink ng-star-inserted&quot;]/div[@class=&quot;row main-box&quot;]/div[@class=&quot;col-10 box2&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-10&quot;]</value>
+      <webElementGuid>d15a8cc9-bd2d-482f-87b6-d509117a64f9</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='F.PERMANENCIA'])[2]/following::div[8]</value>
+      <webElementGuid>947be64a-8c62-420f-8dfb-9a21377722e3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='TIPO | NÚMERO DE OBLIGACIÓN | ENTIDAD'])[2]/following::div[9]</value>
+      <webElementGuid>2b53758c-4466-49ed-9fd5-c1703fdfc393</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='×'])[1]/preceding::div[5]</value>
+      <webElementGuid>e5be5ec4-7576-4fde-a9b7-9f385d8e74aa</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='©2016 Experian Information Solutions, Inc. Experian Marketing Services All rights reserved'])[1]/preceding::div[11]</value>
+      <webElementGuid>26e87399-ccf5-40e4-86d8-8d0c15504387</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a/div/div[2]/div/div</value>
+      <webElementGuid>ac1db08c-594b-4588-99fa-3cc094174287</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'CVE 000000111699097121 VOTRE PASSION  S' or . = 'CVE 000000111699097121 VOTRE PASSION  S')]</value>
+      <webElementGuid>7d909ac6-35ff-4e77-834a-3587c548c0bf</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_Novedat/h3_CVE 000000111699097121 VOTRE PASSION  SB.rs
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_CVE 000000111699097121 VOTRE PASSION  SB</name>
+   <tag></tag>
+   <elementGuidId>f558b901-a843-4f80-80a6-187b8c834a49</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>a.cursorPointerLink > h3</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='F.PERMANENCIA'])[2]/following::h3[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+      <webElementGuid>f6b18a69-86b1-4d10-9c93-4c8519e13d6e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>CVE 000000111699097121 VOTRE PASSION  SB</value>
+      <webElementGuid>bc5ce13d-96c8-463c-af8f-b8b596dcc888</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/article[@class=&quot;main-content&quot;]/app-reactivation-account[@class=&quot;ng-star-inserted&quot;]/div[@class=&quot;ng-star-inserted&quot;]/section[@class=&quot;obligacion-status&quot;]/div[@class=&quot;page-card&quot;]/div[@class=&quot;obligacion-result ng-star-inserted&quot;]/div[@class=&quot;row&quot;]/app-obligations-holder-list2[@class=&quot;appObligList ng-star-inserted&quot;]/div[@class=&quot;col-sm-12 col-12 ng-star-inserted&quot;]/a[@class=&quot;cursorPointerLink&quot;]/h3[1]</value>
+      <webElementGuid>ad7c965a-9f7d-4c39-b59e-4ecb4695962c</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='F.PERMANENCIA'])[2]/following::h3[1]</value>
+      <webElementGuid>6f3a9998-6e28-472e-a8a1-28b12b5bdff0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='TIPO | NÚMERO DE OBLIGACIÓN | ENTIDAD'])[2]/following::h3[1]</value>
+      <webElementGuid>5f9886e4-71b0-4a04-98fb-8cf214a44ddf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a/h3</value>
+      <webElementGuid>c8208897-d4e8-459e-bfea-97ef00dd2291</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h3[(text() = 'CVE 000000111699097121 VOTRE PASSION  SB' or . = 'CVE 000000111699097121 VOTRE PASSION  SB')]</value>
+      <webElementGuid>f9c7e041-3f47-4edb-9a3f-d75a3d73d7dc</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
